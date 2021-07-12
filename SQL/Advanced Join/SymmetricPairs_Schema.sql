c06147f87628786a39e1a301d801742ae65db203
Daniel Ng <daniel.ng@websparks.sg>
2021-07-12 09:04:44 +0800

    MS SQL Solution for Symmetric Pairs

---
+++ SQL/Advanced Join/SymmetricPairs_Schema.sql
@@ -0,0 +1,245 @@
+USE [HackeRank]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Functions](
+	[X] [int] NOT NULL,
+	[Y] [int] NOT NULL
+) ON [PRIMARY]
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (86, 86)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (27, 27)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (45, 45)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (95, 95)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (11, 11)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (18, 8)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (85, 85)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (2, 2)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (77, 77)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (91, 91)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (15, 15)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (84, 84)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (51, 51)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (32, 32)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (35, 35)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (8, 8)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (92, 92)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (67, 67)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (62, 62)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (33, 33)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (13, 13)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (15, 11)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (18, 18)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (3, 3)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (38, 38)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (80, 80)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (34, 34)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (6, 6)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (72, 72)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (14, 12)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (44, 44)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (4, 22)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (90, 90)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (47, 47)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (78, 78)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (23, 3)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (42, 42)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (56, 56)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (79, 79)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (55, 55)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (65, 65)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (17, 17)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (64, 64)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (4, 4)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (28, 28)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (19, 19)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (17, 9)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (36, 36)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (25, 25)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (81, 81)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (60, 60)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (48, 48)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (5, 5)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (88, 88)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (7, 19)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (21, 21)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (29, 29)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (52, 52)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (9, 17)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (9, 9)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (13, 13)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (16, 10)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (1, 1)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (31, 31)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (46, 46)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (7, 7)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (58, 58)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (23, 23)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (87, 87)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (83, 83)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (66, 66)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (93, 93)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (24, 2)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (98, 98)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (53, 53)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (20, 6)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (61, 61)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (20, 20)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (96, 96)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (99, 99)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (73, 73)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (2, 24)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (14, 14)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (71, 71)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (5, 21)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (22, 4)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (75, 75)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (6, 20)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (97, 97)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (41, 41)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (26, 26)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (22, 22)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (8, 18)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (74, 74)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (40, 40)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (21, 5)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (94, 94)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (76, 76)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (49, 49)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (11, 15)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (59, 59)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (89, 89)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (68, 68)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (24, 24)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (37, 37)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (12, 12)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (63, 63)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (43, 43)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (16, 16)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (100, 100)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (39, 39)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (25, 1)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (69, 69)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (54, 54)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (50, 50)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (30, 30)
+GO
+INSERT [dbo].[Functions] ([X], [Y]) VALUES (10, 10)
+GO
